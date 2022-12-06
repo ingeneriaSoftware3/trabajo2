12cdc7466805cef459077937e4e2ae4f418d2072
Pedro Figueroa <pedro.figueroa.c@usach.cl>
2022-12-06 20:47:10 -0300

    Agregando soluciónes a 1.4, 1.1 y 1.3

--- prueba1.6.sh
+++ prueba1.6.sh
@@ -1,3 +1,8 @@
-ffmpeg -i $1 -vn -f f32le -acodec pcm_f32le -ac 2 output.raw
+#!/bin/bash
 
-ffmpeg -f f32le -ac 2 -i output.raw audio.$2
+ffmpeg -i $1 -vn -format f32le -acodec pcm_f32le -vn -c:a copy output.mp3
+
+
+#ffmpeg -i output.raw -f f32le -ac 2 audio.$2
+
+ffmpeg -i $1 -vf scale=1280:720 -preset slow -crf 18 output6.$2
