17b93ae7ff984adad58c2c52d39c76755eea1b8a
ser-mei <71984487+ser-mei@users.noreply.github.com>
2022-12-10 22:16:22 -0300

    prueba 1.6 actualizada

--- prueba1.6.sh
+++ prueba1.6.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-ffmpeg -i $1 -vn -format f32le -acodec pcm_f32le -vn -c:a copy output.mp3
-
-
-#ffmpeg -i output.raw -f f32le -ac 2 audio.$2
-
-ffmpeg -i $1 -vf scale=1280:720 -preset slow -crf 18 output6.$2
+ffmpeg -f f32le -ac 2 -i $1 audio.$2
