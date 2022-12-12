01f14f235c8a966ad0dd098b92141e77b59df719
IgnacioSanhuezaZ <45337800+IgnacioSanhuezaZ@users.noreply.github.com>
2022-12-11 22:44:13 -0300

    Estructura básica del bash script lista
    Falta terminar la verificación de cada uno de las variables en cuestión con los rangos ya obtenidos

--- Act1.sh
+++ Act1.sh
@@ -3,9 +3,10 @@
 # 
 
 # 3.from library ffmpeg, change aspect ratio 16:9 to 4:3 and 4:3 to 16:9
-#   ffmpeg -i input.mp4 -aspect 4:3 output.mp4
-#   ffmpeg -i input.mp4 -aspect 16:9 output.mp4
+#   ffmpeg -i $1 -aspect 4:3 output_4_3.mp4
+#   ffmpeg -i $1 -aspect 16:9 output_16_9.mp4
 
 
 # 5.from library ffmpeg, extract audio from video
-#   ffmpeg -i input-video.avi -vn -acodec copy output-audio.aac
+ffmpeg -i $1 -vn -f f32le -acodec pcm_f32le -ac 2 output-audio.raw
+#ffmpeg -i $1 -vn -acodec copy output-audio.raw
--- Unificacion de respuestas.bash
+++ Unificacion de respuestas.bash
@@ -7,46 +7,84 @@
 # Hay que realizar las verificaciónes de cada argumento de entrada para lanzar error o cortar la ejecución 
 
 #Separar extensión del archivo de video para que haya consistencia en la extensión de salida
-arrIN=(${1//./ })
-OUT_FORMAT=${arrIN[1]} 
-#Extracción 
-FORMATOPIXELES=| exiftool 4kvideo.mp4 | grep 'Image Size'
 
-echo $FORMATOPIXELES | sed 's:^::; /: / s:::'
-# Set comma as delimiter
-IFS=': '
-echo $arrIN
-#Read the split words into an array based on comma delimiter
-readarray -d _ -t arr <<<"$FORMATOPIXELES"
-VAR=| read $FORMATOPIXELES
-read -a arrIN <<< "$VAR"
-echo $arrIN
+USSAGE_MSG="USAGE: bash Unificaion\ de\ respuestas.bash [dirección al archivo de video mp4] [Si corre la prueba de transcodificación (1), de algoritmo distribuido (2), o ambas(3)]
+                                                        [número de hilos sobre los cuales correr el algoritmo distribuído (si no aplica = 0, si desea todos = -1)]
+                                                        [tamaño de salida del video (800x600 | 640x480 | etc ...)] [tiempo de inicio del corte al video] [tiempo de corte al video]
+                                                        [posición superior izquerda para el recorte del video] [posición inferior derecha para el recorte del video]
+                                                        [formato de salida para el video (ogg/mp3/flac)]
+            Ejemplos:
+                    Ejecución de sólo las pruebas de transcodificación:
+                    bash Unificaion\ de\ respuestas.bash 4kvideo.mp4 1 0 800x600  00:03:00  00:05:00 960:540 480:270 mp3
 
-FORMATOPIXELES=${arrIN[1]}
-# ----if para ver si se corre la prueba 1, 2 o ambas-----
+                    Ejecución de sólo la prueba de algoritmo distribuído:
+                    bash Unificaion\ de\ respuestas.bash 4kvideo.mp4 2 -1
 
-# if $<índice donde quede la variable> == 1 || 3
+                    Ejecución de ambas pruebas:
+                    bash Unificaion\ de\ respuestas.bash 4kvideo.mp4 3 4 1280x1024 00:00:00  01:02:00 0:0 680:270 ogg
+                    "
+NOT_VALID_YET=0
+if ((($2 == "1" | $2 == "3") & $# == "9"))
+then
+    arrIN=(${1//./ })
+    OUT_FORMAT=${arrIN[1]} 
+    #Extracción de rangos para los parámetros
+    WIDE_INFO=$(exiftool 4kvideo.mp4 | grep 'Image Width'  )
+    HEIGHT_INFO=$(exiftool 4kvideo.mp4 | grep 'Image Height')
+    DURACIONVIDEO_INFO=$(exiftool 4kvideo.mp4 | grep 'Media Duration')
+    #Rango tamaño del video
+    arrIN2=(${WIDE_INFO//:/ })
+    SIZE_WIDE=${arrIN2[2]}
+    arrIN3=(${HEIGHT_INFO//:/ })
+    SIZE_HEIGHT=${arrIN3[2]}
+    #Rango duración del video
+    arrIN4=(${DURACIONVIDEO_INFO//:/ })
+    DURACIONVIDEO=(${arrIN4[2]})
 
-#1.1 Cambio de resolución: A full HD, HD y un tamaño menor, a elección (800x600, 640x480, etc)
-#ffmpeg -i $1 -vf scale=1280:720 -preset slow -crf 18 "output6."$OUT_FORMAT
+    #DEBUG----
+    echo "Duración del video: "$DURACIONVIDEO
+    echo "Alto del video en pixeles: "$SIZE_WIDE
+    echo "Ancho del video en pixeles: "$SIZE_HEIGHT
+    echo $1
+    #---------
 
-#1.2 Cambio de tasa de aspecto 16:9 a 5:4 y viceversa.
-ffmpeg -i $1 -aspect 4:3 output4_3.mp4
-ffmpeg -i $1 -aspect 16:9 output16_9.mp4
+    #1.1 Cambio de resolución: A full HD, HD y un tamaño menor, a elección (800x600, 640x480, etc)
+    arrIN4=(${4//x/ })
+    if (($($arrIN4[0] -le $SIZE_WIDE) & $($arrIN4[1] -le $SIZE_HEIGHT)))
+       then
+           ffmpeg -i $1 -vf scale=$4 -preset slow -crf 18 "output"$4"."$OUT_FORMAT
+    else
+        echo "Wrong size "$4", size must be equal or less than the original resolution "$SIZE_WIDE"x"$SIZE_HEIGT
 
-# 1.3 extracción de un fragmento del video en tiempo
-#ffmpeg -i $1 -map 0 -default_mode infer_no_subs -ss 00:03:00 -to 00:10:00 -c copy "Output."$2
+    #1.2 Cambio de tasa de aspecto 16:9 a 5:4 y viceversa.
+    #ffmpeg -i $1 -aspect 4:3 output4_3.$OUT_FORMAT
+    #ffmpeg -i $1 -aspect 16:9 output16_9.$OUT_FORMAT
 
-# 1.4 Recorte de un área rectangular del video, escogiendo punto superior izquierdo e inferior derecho
-ffmpeg -i $1 -vf crop=960:540:480:270 "recorte."$OUT_FORMAT
+    # 1.3 extracción de un fragmento del video en tiempo
+    #ffmpeg -i $1 -map 0 -default_mode infer_no_subs -ss $5 -to $6 -c copy "Output_Fragment."$OUT_FORMAT
 
-#1.5 Extracción de audio de parte del video a formato raw
-ffmpeg -i input-video.avi -vn -acodec copy output-audio.aac
+    # 1.4 Recorte de un área rectangular del video, escogiendo punto superior izquierdo e inferior derecho
+    #ffmpeg -i $1 -vf crop=$7":"$8 "recorte."$OUT_FORMAT
 
-#1.6 Codificación del audio a flac/ogg/mp3
-ffmpeg -f f32le -ac 2 -i $1 audio.$<variable en la que quede el parámetro>
+    #1.5 Extracción de audio de parte del video a formato raw
+    #ffmpeg -i $1 -vn -f f32le -acodec pcm_f32le -ac 2 output-audio.raw
 
-# if $<índice donde quede la variable> == 2 || 3
+    #1.6 Codificación del audio a flac/ogg/mp3
+    #ffmpeg -f f32le -ac 2 -i output-audio.raw output-audio.$9
+    #fi
+    #if ($2 -eq 2 | $2 -eq 3)
+    #then
+    #2 Programa mpicc
+    #fi
+    #if ($2 -lt 1 & $2 -gt 3)
+    #then
+else
+    NOT_VALID_YET=1
+fi
 
-#2 Programa mpicc
-
+if (($2 != "1" & (($2 == "2" & $# == "3") | ($2 == "3" & $# == 9))))
+then
+    echo "Programa mpicc en camino..."
+else
+    echo $USAGE_MSG
+fi
