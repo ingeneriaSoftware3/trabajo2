2edffd57e4a594194749cbdb0fca59f409c5f7ec
IgnacioSanhuezaZ <45337800+IgnacioSanhuezaZ@users.noreply.github.com>
2022-12-12 00:14:59 -0300

    Estructura básica lista junto a verificación de parámetros, sólo falta verificación de 1.3

--- Unificacion de respuestas.bash
+++ Unificacion de respuestas.bash
@@ -8,22 +8,6 @@
 
 #Separar extensión del archivo de video para que haya consistencia en la extensión de salida
 
-USSAGE_MSG="USAGE: bash Unificaion\ de\ respuestas.bash [dirección al archivo de video mp4] [Si corre la prueba de transcodificación (1), de algoritmo distribuido (2), o ambas(3)]
-                                                        [número de hilos sobre los cuales correr el algoritmo distribuído (si no aplica = 0, si desea todos = -1)]
-                                                        [tamaño de salida del video (800x600 | 640x480 | etc ...)] [tiempo de inicio del corte al video] [tiempo de corte al video]
-                                                        [posición superior izquerda para el recorte del video] [posición inferior derecha para el recorte del video]
-                                                        [formato de salida para el video (ogg/mp3/flac)]
-            Ejemplos:
-                    Ejecución de sólo las pruebas de transcodificación:
-                    bash Unificaion\ de\ respuestas.bash 4kvideo.mp4 1 0 800x600  00:03:00  00:05:00 960:540 480:270 mp3
-
-                    Ejecución de sólo la prueba de algoritmo distribuído:
-                    bash Unificaion\ de\ respuestas.bash 4kvideo.mp4 2 -1
-
-                    Ejecución de ambas pruebas:
-                    bash Unificaion\ de\ respuestas.bash 4kvideo.mp4 3 4 1280x1024 00:00:00  01:02:00 0:0 680:270 ogg
-                    "
-NOT_VALID_YET=0
 if ((($2 == "1" | $2 == "3") & $# == "9"))
 then
     arrIN=(${1//./ })
@@ -50,41 +34,65 @@ then
 
     #1.1 Cambio de resolución: A full HD, HD y un tamaño menor, a elección (800x600, 640x480, etc)
     arrIN4=(${4//x/ })
-    if (($($arrIN4[0] -le $SIZE_WIDE) & $($arrIN4[1] -le $SIZE_HEIGHT)))
-       then
-           ffmpeg -i $1 -vf scale=$4 -preset slow -crf 18 "output"$4"."$OUT_FORMAT
-    else
-        echo "Wrong size "$4", size must be equal or less than the original resolution "$SIZE_WIDE"x"$SIZE_HEIGT
-
+    WIDE=${arrIN4[0]}
+    echo $WIDE
+    HEIGHT=${arrIN4[1]}
+    echo $HEIGHT
+    echo 
+    #if (($WIDE <= $SIZE_WIDE))
+     #  then
+      #     if (($HEIGHT <= $SIZE_HEIGHT))
+       #       then
+        #          ffmpeg -i $1 -vf scale=$4 -preset slow -crf 18 "output"$4"."$OUT_FORMAT
+         #  else
+          #     echo "Wrong size "$4", size must be equal or less than the original resolution "$SIZE_WIDE"x"$SIZE_HEIGHT
+           #fi
+#    else
+ #       echo "Wrong size "$4", size must be equal or less than the original resolution "$SIZE_WIDE"x"$SIZE_HEIGHT
+  #  fi
     #1.2 Cambio de tasa de aspecto 16:9 a 5:4 y viceversa.
-    #ffmpeg -i $1 -aspect 4:3 output4_3.$OUT_FORMAT
+   # ffmpeg -i $1 -aspect 4:3 output4_3.$OUT_FORMAT
     #ffmpeg -i $1 -aspect 16:9 output16_9.$OUT_FORMAT
 
     # 1.3 extracción de un fragmento del video en tiempo
-    #ffmpeg -i $1 -map 0 -default_mode infer_no_subs -ss $5 -to $6 -c copy "Output_Fragment."$OUT_FORMAT
-
+    # -------Falta convertir el tiempo en formato hh:mm:ss a segundos en Int y cortar la duración del video en segundos a Int igual
+    if (($5 <= $DURACIONVIDEO & $6 <= $DURACIONVIDEO))
+       then
+           ffmpeg -i $1 -map 0 -default_mode infer_no_subs -ss $5 -to $6 -c copy "Output_Fragment."$OUT_FORMAT
+    else
+        echo "Wrong cutting time starting in: "$5" and enging in: "$6", the start and end of the cut must be less or equal than the original lenght: "$DURACIONVIDEO
+    fi
     # 1.4 Recorte de un área rectangular del video, escogiendo punto superior izquierdo e inferior derecho
-    #ffmpeg -i $1 -vf crop=$7":"$8 "recorte."$OUT_FORMAT
-
+    if (($7 <= $SIZE_WIDE & $8 <= $SIZE_HEIGHT))
+       then
+           ffmpeg -i $1 -vf crop=$7":"$8 "recorte."$OUT_FORMAT
+    else
+        echo "Wrong size "$7" or "$8", size of both points must be equal or less than the original resolution "$SIZE_WIDE"x"$SIZE_HEIGHT
+    fi
     #1.5 Extracción de audio de parte del video a formato raw
-    #ffmpeg -i $1 -vn -f f32le -acodec pcm_f32le -ac 2 output-audio.raw
+    ffmpeg -i $1 -vn -f f32le -acodec pcm_f32le -ac 2 output-audio.raw
 
     #1.6 Codificación del audio a flac/ogg/mp3
-    #ffmpeg -f f32le -ac 2 -i output-audio.raw output-audio.$9
-    #fi
-    #if ($2 -eq 2 | $2 -eq 3)
-    #then
-    #2 Programa mpicc
-    #fi
-    #if ($2 -lt 1 & $2 -gt 3)
-    #then
-else
-    NOT_VALID_YET=1
+    ffmpeg -f f32le -ac 2 -i output-audio.raw output-audio.$9
 fi
 
 if (($2 != "1" & (($2 == "2" & $# == "3") | ($2 == "3" & $# == 9))))
 then
     echo "Programa mpicc en camino..."
 else
-    echo $USAGE_MSG
+    echo "USAGE: bash Unificaion\ de\ respuestas.bash [dirección al archivo de video mp4] [Si corre la prueba de transcodificación (1), de algoritmo distribuido (2), o ambas(3)]"
+    echo "                                                        [número de hilos sobre los cuales correr el algoritmo distribuído (si no aplica = 0, si desea todos = -1)]"
+    echo "                                                        [tamaño de salida del video (800x600 | 640x480 | etc ...)] [tiempo de inicio del corte al video] [tiempo de corte al video]"
+    echo "                                                        [posición superior izquerda para el recorte del video] [posición inferior derecha para el recorte del video]"
+    echo "                                                        [formato de salida para el video (ogg/mp3/flac)]"
+    echo "            Ejemplos:"
+    echo "                    Ejecución de sólo las pruebas de transcodificación:"
+    echo "                    bash Unificaion\ de\ respuestas.bash 4kvideo.mp4 1 0 800x600  00:03:00  00:05:00 960:540 480:270 mp3"
+    echo ""
+    echo "                    Ejecución de sólo la prueba de algoritmo distribuído:"
+    echo "                    bash Unificaion\ de\ respuestas.bash 4kvideo.mp4 2 -1"
+    echo ""
+    echo "                    Ejecución de ambas pruebas:"
+    echo "                    bash Unificaion\ de\ respuestas.bash 4kvideo.mp4 3 4 1280x1024 00:00:00  01:02:00 0:0 680:270 ogg"
 fi
+
